326cd303fe5a928ff3a823e3887e6e2c665a7b09
Alok soni <db.aloksoni@gmail.com>
2023-08-30 23:07:51 -0700

    design contact modify css and dropify css and js

--- documents.php
+++ documents.php
@@ -11,7 +11,7 @@
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="HandheldFriendly" content="True">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-    <title>Login-clearstarttax</title>
+    <title>upload documents</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- :: Main CSS-->
