e0b945d4c9be1ddb176bf7cf567012415b4bb6a1
Alok soni <db.aloksoni@gmail.com>
2023-09-04 06:38:15 -0700

    add link

--- include/header.php
+++ include/header.php
@@ -90,7 +90,7 @@
             </li>
             <!-- :: menu items 04 -->
             <li>
-                <a class="app-menu__item" href="Appointment.php">
+                <a class="app-menu__item" href="appointment.php">
                     <svg class="app-menu__icon mx-3" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                         viewBox="0 0 24 24">
                         <g fill="none">
