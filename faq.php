3aa502b8a1b86169be3692115eb208bfed6d4c75
Alok soni <db.aloksoni@gmail.com>
2023-09-07 07:22:28 -0700

    work on updates and

--- faq.php
+++ faq.php
@@ -10,7 +10,7 @@
 		<meta name="mobile-web-app-capable" content="yes">
 		<meta name="HandheldFriendly" content="True">
 		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-		<title>Login-clearstarttax</title>
+		<title>faq</title>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- Main CSS-->
